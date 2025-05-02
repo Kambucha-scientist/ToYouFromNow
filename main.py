d166dcfa35087d6ba2f94c5eab9e58cca8652b82
Kambucha-scientist <123koneva@gmail.com>
2025-05-02 22:01:44 +0400

    main.py updated

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from aiogram import Bot
+import asyncio
+from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 import os
+
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('sup <3!')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
