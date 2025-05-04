4762c8e41bda5b98dec43ac2b5104376bf3137ae
Kambucha-scientist <123koneva@gmail.com>
2025-05-04 19:16:20 +0400

    delete legacy

--- databaseInterface.py
+++ databaseInterface.py
@@ -1,37 +1,3 @@
-"""
-import sqlite3 as sl
-
-
-def createTable():
-    con = sl.connect('messages.db')
-    cursor = con.cursor()
-    with con:
-        cursor.execute("CREATE TABLE IF NOT EXISTS messages(user_id BIGINT NOT NULL, chat_id BIGINT NOT NULL, "
-                       "text TEXT NOT NULL, sending_time TIMESTAMP NOT NULL, is_sent INT default 0)")
-    con.commit()
-    con.close()
-
-
-def getUnsentMessage():
-    con = sl.connect('messages.db')
-    cursor = con.cursor()
-    with con:
-        cursor.execute("SELECT * from messages WHERE is_sent = 0")
-        messages = cursor.fetchall()
-    con.commit()
-    con.close()
-    return messages
-
-
-def clear():
-    con = sl.connect('messages.db')
-    cursor = con.cursor()
-    with con:
-        cursor.execute("DELETE FROM messages WHERE is_sent = 1")
-    con.commit()
-    con.close()
-"""
-
 from sqlalchemy import BigInteger, TIMESTAMP, Text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
